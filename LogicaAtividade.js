ea19d75e774bc9c5fb93e01f7fd60e0914f045f4
Felipe <fsantos.159852@gmail.com>
2021-10-31 21:30:42 -0300

    Atividade encontro remoto 2 Atualizada

--- LogicaAtividade.js
+++ LogicaAtividade.js
@@ -1,25 +1,37 @@
 //Atividade de lógica de programação encontro remoto 1//
 
-var ListaDeHardware = [
-  "Processador",
-  "Placa Mãe",
-  "Placa de Vídeo",
-  "HD Sata",
-  "HD Ssd",
-  "Memoria Ram",
-  "Fan Cooler",
-  "Fonte de Alimentação",
-  "Monitor Led",
-  "Caixas de som",
-  "Mouse",
-  "Teclado",
-  "Headset",
-];
+var listaDePecas = ["Engrenagens", "Motor", "Freios", "Caixa de transmissão", "filtro de ar", "Eixos", "Diferencial", "Bomba de gasolina"];
 
-if (ListaDeHardware.length < 10) {
-  console.log("Sua máquina não contem Hardwere sufucientes para funcionar.");
-} else if (ListaDeHardware.length == 13) {
-  console.log("Sua máquina está completa para uso.");
+if (listaDePecas.length > 10) {
+    console.log("Não tem mais espaço nessa lista, impossível cadastrar.");
 } else {
-  console.log("Máquina utilizável");
+    console.log("É possível cadastrar mais peças.");
+}
+
+var pesoDasPecas = "";
+
+if (pesoDasPecas < 100 ) {
+    console.log("A peça deve possuir pelo menos 100 gramas.");
+} else {
+    console.log("A peça possui peso adequado.");
+}
+
+// Quantidade de caracteres com switch case
+
+nomeDasPecas = "";
+
+switch (nomeDasPecas.length) {
+    case 0:
+        console.log("O nome da peça não pode ser vazio.");
+        break;
+    
+    case 1:
+    case 2:
+    case 3:
+        console.log("O nome deve conter mais de 3 caracteres, digite um nome adequado.");
+        break;
+
+    default:
+        console.log("O nome está adequado para cadastro.");
+        break;
 }
